d3178cccb8ea8a740202ff5d0005080335a12c63
Kiruthiga Manivannan <kiruthiga.manivannan@evry.com>
2019-12-04 16:56:15 +0100

    Added Day4

---
+++ Day3/AOC_day3/AOC_day3/Program.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AOC_day3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string input1 = "R8,U5,L5,D3";//"R75,D30,R83,U83,L12,D49,R71,U7,L72";
+            //string input2 = "U7,R6,D4,L4"; //"U62,R66,U55,R34,D71,R55,D58,R83";
+            //string input1 = "R75,D30,R83,U83,L12,D49,R71,U7,L72";
+            //string input2 = "U62,R66,U55,R34,D71,R55,D58,R83";
+            string input1 = "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51";
+            string input2 = "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7";
+            CalculateDistance(input1, input2);
+
+        }
+
+        public static void CalculateDistance(string input1, string input2)
+        {
+
+            string[] inputs1 = input1.Split(',');
+            int x1 = 0;
+            int y1 = 0;
+            string[] wire1 = new string[inputs1.Length+1];
+            wire1.SetValue("0,0",0);
+            for (int i = 0; i < inputs1.Length; i++)
+            {
+                if (inputs1[i].StartsWith("R"))
+                {
+                    x1 = x1 + Convert.ToInt32(inputs1[i].Replace("R",""));
+                }
+                if (inputs1[i].StartsWith("L"))
+                {
+                    x1 = x1 - Convert.ToInt32(inputs1[i].Replace("L", ""));
+                }
+                if (inputs1[i].StartsWith("U"))
+                {
+                    y1 = y1 + Convert.ToInt32(inputs1[i].Replace("U", ""));
+                }
+                if (inputs1[i].StartsWith("D"))
+                {
+                    y1 = y1 - Convert.ToInt32(inputs1[i].Replace("D", ""));
+                }
+                wire1.SetValue(x1.ToString()+","+y1.ToString(),i+1);
+            }
+            Console.WriteLine("wire1: "+ string.Join("/",wire1));
+
+
+            string[] inputs2 = input2.Split(',');
+            int x2 = 0;
+            int y2 = 0;
+            string[] wire2 = new string[inputs2.Length+1];
+            wire2.SetValue("0,0", 0);
+            for (int i = 0; i < inputs2.Length; i++)
+            {
+                if (inputs2[i].StartsWith("R"))
+                {
+                    x2 = x2 + Convert.ToInt32(inputs2[i].Replace("R", ""));
+                }
+                if (inputs2[i].StartsWith("L"))
+                {
+                    x2 = x2 - Convert.ToInt32(inputs2[i].Replace("L", ""));
+                }
+                if (inputs2[i].StartsWith("U"))
+                {
+                    y2 = y2 + Convert.ToInt32(inputs2[i].Replace("U", ""));
+                }
+                if (inputs2[i].StartsWith("D"))
+                {
+                    y2 = y2 - Convert.ToInt32(inputs2[i].Replace("D", ""));
+                }
+                wire2.SetValue(x2.ToString() + "," + y2.ToString(), i+1);
+            }
+            Console.WriteLine("wire2: " + string.Join("/", wire2));
+
+            int[] A1 = new int[wire1.Length];
+            int[] B1 = new int[wire1.Length];
+            int[] C1 = new int[wire1.Length];
+            string[] firstLine= new string[wire1.Length];
+
+            for (int i=0;i<wire1.Length-1;i++)
+            {
+                string[] a = wire1[i].Split(',');
+                string[] b = wire1[i+1].Split(',');
+
+                A1[i] = Convert.ToInt32(b[1]) - Convert.ToInt32(a[1]);
+                B1[i] = Convert.ToInt32(a[0]) - Convert.ToInt32(b[0]);
+                C1[i] = A1[i] * Convert.ToInt32(b[0]) + B1[i] * Convert.ToInt32(b[1]);
+
+                firstLine.SetValue(A1[i].ToString()+","+B1[i].ToString() + "," + C1[i].ToString(), i);
+            }
+            Console.WriteLine("firstlines: " + string.Join("/", firstLine));
+
+            int[] A2 = new int[wire2.Length];
+            int[] B2 = new int[wire2.Length];
+            int[] C2 = new int[wire2.Length];
+            string[] secondLine = new string[wire2.Length];
+
+            for (int i = 0; i < wire2.Length - 1; i++)
+            {
+                string[] a = wire2[i].Split(',');
+                string[] b = wire2[i + 1].Split(',');
+
+                A2[i] = Convert.ToInt32(b[1]) - Convert.ToInt32(a[1]);
+                B2[i] = Convert.ToInt32(a[0]) - Convert.ToInt32(b[0]);
+
+                C2[i] = A2[i] * Convert.ToInt32(b[0]) + B2[i] * Convert.ToInt32(b[1]);
+
+                secondLine.SetValue(A2[i].ToString() + "," + B2[i].ToString() + "," + C2[i].ToString(), i);
+            }
+            Console.WriteLine("secondLines: " + string.Join("/", secondLine));
+
+            string[] intersections = new string[firstLine.Length - 1];
+
+            for (int j = 0; j < firstLine.Length-1; j++)
+            {
+                if (secondLine[j] != null && firstLine[j] != null)
+                {
+                    string[] first = firstLine[j].Split(',');
+                    string[] second = secondLine[j].Split(',');
+
+                    double delta = Convert.ToInt32(first[0]) * Convert.ToInt32(second[1]) -
+                                   Convert.ToInt32(first[1]) * Convert.ToInt32(second[0]);
+
+                    if (delta == 0)
+                    {
+                        //paralell
+                    }
+                    else
+                    {
+                            double x = (Convert.ToInt32(second[1]) * Convert.ToInt32(first[2]) -
+                                     Convert.ToInt32(first[1]) * Convert.ToInt32(second[2])) / delta;
+                            double y = (Convert.ToInt32(first[0]) * Convert.ToInt32(second[2]) -
+                                     Convert.ToInt32(second[0]) * Convert.ToInt32(first[2])) / delta;
+
+                            intersections.SetValue(x.ToString() + "," + y.ToString(), j);
+
+                        }
+                    }
+
+            }
+            
+            Console.WriteLine("intersections: " + string.Join("/", intersections));
+
+            int minDistance = 0;
+
+            for (int j = 0; j < intersections.Length; j++)
+            {
+                if (intersections[j] != null)
+                {
+                    string[] xy = intersections[j].Split(',');
+                    string[] w1 = wire1[j].Split(',');
+                    string[] w2 = wire2[j].Split(',');
+
+                    if (xy[0] != null && Convert.ToInt32(xy[0]) >=0 && Convert.ToInt32(xy[1]) >=  0)
+                    {
+                        if (Convert.ToInt32(xy[0]) <= Convert.ToInt32(w1[0]) && Convert.ToInt32(xy[0]) <= Convert.ToInt32(w2[0])
+                        && Convert.ToInt32(xy[1]) <= Convert.ToInt32(w1[1]) && Convert.ToInt32(xy[1]) <= Convert.ToInt32(w2[1]))
+                        {
+
+                            int distance = Convert.ToInt32(xy[0]) + Convert.ToInt32(xy[1]);
+                            if (minDistance == 0)
+                            {
+                                minDistance = distance;
+                            }
+                            else
+                            {
+                                if (distance < minDistance)
+                                {
+                                    minDistance = distance;
+                                }
+                            }
+
+                            Console.WriteLine("minDistance: " + minDistance.ToString());
+                        }
+
+                    }
+                }
+
+            }
+
+            Console.WriteLine("minDistance: " + minDistance.ToString());
+
+
+            //https://stackoverflow.com/questions/4543506/algorithm-for-intersection-of-2-lines
+            //https://www.topcoder.com/community/competitive-programming/tutorials/geometry-concepts-line-intersection-and-its-applications/
+
+            
+        }
+    }
+}
---
+++ Day4/AOC_Day4/AOC_Day4/Program.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AOC_Day4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input1 = 245318;
+            int input2 = 765747;
+
+            int num6 = (input1 / 1) % 10;
+            int num5 = (input1 / 10) % 10;
+            int num4 = (input1 / 100) % 10;
+            int num3 = (input1 / 1000) % 10;
+            int num2 = (input1 / 10000) % 10;
+            int num1 = (input1 / 100000) % 10;
+
+            //bool iscorrect = CheckRule(num1, num2, num3, num4, num5, num6);
+
+            //Console.WriteLine("Is correct "+iscorrect.ToString());
+            Console.WriteLine("Numbers: " + num1 + num2 + num3 + num4 + num5 + num6);
+
+            loops(num1, num2, num3, num4, num5, num6);
+
+
+
+        }
+        public static void loops(int num1, int num2, int num3, int num4, int num5, int num6)
+        {
+            int counts = 0;
+            bool resetLoop1 = false;
+            bool resetLoop2 = false;
+            bool resetLoop3 = false;
+            bool resetLoop4 = false;
+            bool resetLoop5 = false;
+            for (int i = num1; i <= 7; i++)
+            {
+                if (resetLoop1 == true)
+                {
+                    num2 = 0;
+                    resetLoop1 = false;
+                }
+
+                for (int j = num2; j <= 9; j++)
+                {
+                    if (resetLoop2 == true)
+                    {
+                        num3 = 0;
+                        resetLoop2 = false;
+                    }
+
+                    for (int k = num3; k <= 9; k++)
+                    {
+                        if (resetLoop3 == true)
+                        {
+                            num4 = 0;
+                            resetLoop3 = false;
+                        }
+
+                        for (int l = num4; l <= 9; l++)
+                        {
+                            if (resetLoop4 == true)
+                            {
+                                num5 = 0;
+                                resetLoop4 = false;
+                            }
+
+                            for (int m = num5; m <= 9; m++)
+                            {
+                                if (resetLoop5 == true)
+                                {
+                                    num6 = 0;
+                                    resetLoop5 = false;
+                                }
+
+                                for (int n = num6; n <= 9; n++)
+                                {
+
+                                    string number = i.ToString() + j.ToString() + k.ToString() + l.ToString() + m.ToString() + n.ToString();
+                                    if (Convert.ToInt32(number) > 765747)
+                                    {
+                                        return;
+                                    }
+
+                                    if (CheckRule(i, j, k, l, m, n))
+                                    {
+                                        counts++;
+                                        Console.WriteLine("counts " + counts);
+                                        Console.WriteLine(number);
+                                    }
+
+                                }
+                                resetLoop5 = true;
+
+                            }
+                            resetLoop4 = true;
+                        }
+                        resetLoop3 = true;
+                    }
+
+                    resetLoop2 = true;
+                }
+
+                resetLoop1 = true;
+            }
+        }
+
+        public static bool CheckRule(int num1, int num2, int num3, int num4, int num5, int num6)
+        {
+            bool iscorrect = false;
+            bool hasdouble = false;
+            bool hasdecrease = false;
+            bool hastriple = false;
+            int[] doubleInts = new int[5];
+            int[] numbers = new int[6] {num1, num2, num3, num4, num5, num6};
+            for (int i = 0; i < 5; i++)
+            {
+                if (numbers[i] == numbers[i + 1])
+                {
+                    
+                    hasdouble = true;
+                    if(i>0)
+                    {
+                        if (numbers[i] == numbers[i - 1])
+                        {
+                            hasdouble = false;
+                        }
+                    }
+                }
+
+                if (i < 4)
+                {
+                    if (numbers[i] == numbers[i + 1] && numbers[i] == numbers[i + 2])
+                    {
+                        hasdouble = false;
+                        hastriple = true;
+                    }
+                }
+
+                if (numbers[i + 1] < numbers[i])
+                {
+                    hasdecrease = true;
+                }
+
+                if (hasdouble)
+                {
+                    foreach (int val in doubleInts)
+                    {
+                        if (val != numbers[i])
+                        {
+                            doubleInts.SetValue(numbers[i], i);
+                        }
+                    }
+                }
+                hasdouble = false;
+            }
+
+            foreach (int val in doubleInts)
+            {
+                if (val != 0)
+                {
+                    hasdouble = true;
+                }
+            }
+
+            if (hasdouble)
+            {
+                hastriple = false;
+            }
+
+            if (hasdouble && !hasdecrease && !hastriple)
+            {
+                iscorrect = true;
+            }
+
+            return iscorrect;
+        }
+    }
+}
